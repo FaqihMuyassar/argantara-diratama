17c52663aa93b23747dbcaea16936a8056b36a9b
FaqihMuyassar <148945703+FaqihMuyassar@users.noreply.github.com>
2025-08-21 12:26:30 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+        noto: ['"Noto Serif"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
